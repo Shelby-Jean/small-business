701a2df7c46ff3c8c25259dc7f20b3adbcb428d6
Shelby-Jean <shelby.gottschall@gmail.com>
2021-01-13 11:13:29 -0500

    useState and useHistory for Add page to redirect to Listings

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,74 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 import {v4 as uuid} from 'uuid';
 import { Container, TextField, Button } from '@material-ui/core';
 // import GoogleMap from './GoogleMap';
 
-class Add extends React.Component {
-  state = {
-    id: 0,
-    name: '',
-    address: '',
-    hours: '',
-    description: ''
+const Add = (props) => {
+  const [id, setId] = useState(0);
+  const [newListing, setNewListing] = useState({});
+
+  let history = useHistory();
+
+  const handleTextChange = (e) => {
+    setId(uuid());
+    setNewListing({ ...newListing, [e.target.name]: e.target.value });
   }
 
-  handleTextChange = (e) => {
-    this.setState({
-      id: uuid(),
-      [e.target.name]: e.target.value
-    })
-  }
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const listing = this.state;
-    this.props.addListing(listing);
+    const listing = {id, ...newListing}
+    props.addListing(listing);
+    history.push("./listings");
   }
 
-  render() {
-    return (
-      <div>
-        <Container maxWidth="lg" style={{display: "flex"}}>
-          <Container maxWidth="sm">
-            <form className="add-form" onSubmit={this.handleSubmit}>
-              <TextField 
-                placeholder="Name" 
-                type="text" name="name" 
-                value={this.state.name} 
-                onChange={this.handleTextChange} 
-              />
-              <TextField 
-                placeholder="Address" 
-                type="text" 
-                name="address" 
-                value={this.state.address} 
-                onChange={this.handleTextChange} 
-              />
-              <TextField 
-                placeholder="Hours (ex: 8AM - 9PM)" 
-                type="text" 
-                name="hours" 
-                value={this.state.hours} 
-                onChange={this.handleTextChange} 
-              />
-              <TextField 
-                placeholder="Description" 
-                type="text" 
-                name="description" 
-                value={this.state.description} 
-                onChange={this.handleTextChange} 
-              />
-              <br />
-              <Button variant="contained" color="secondary" type="submit">SAVE</Button>
-            </form>
-          </Container>
-          <Container maxWidth="sm">
-            {/* <GoogleMap /> */}
-          </Container>
+  return (
+    <div>
+      <Container maxWidth="lg" style={{display: "flex"}}>
+        <Container maxWidth="sm">
+          <form className="add-form" onSubmit={handleSubmit}>
+            <TextField 
+              placeholder="Name" 
+              type="text" name="name" 
+              value={newListing.name} 
+              onChange={handleTextChange} 
+            />
+            <TextField 
+              placeholder="Address" 
+              type="text" 
+              name="address" 
+              value={newListing.address} 
+              onChange={handleTextChange} 
+            />
+            <TextField 
+              placeholder="Hours (ex: 8AM - 9PM)" 
+              type="text" 
+              name="hours" 
+              value={newListing.hours} 
+              onChange={handleTextChange} 
+            />
+            <TextField 
+              placeholder="Description" 
+              type="text" 
+              name="description" 
+              value={newListing.description} 
+              onChange={handleTextChange} 
+            />
+            <br />
+            <Button variant="contained" color="secondary" type="submit">SAVE</Button>
+          </form>
         </Container>
-      </div>
-    )
-  }
+        <Container maxWidth="sm">
+          {/* <GoogleMap /> */}
+        </Container>
+      </Container>
+    </div>
+  )
 }
 
 export default Add;
