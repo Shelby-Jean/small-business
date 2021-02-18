94b12bac2ebfbd6b9c02d72ab677f586b7b9f2d1
Shelby-Jean <shelby.gottschall@gmail.com>
2021-02-18 18:44:33 -0500

    updated server to build files

--- server/server.js
+++ server/server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const port = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, '..', 'public');
+const buildPath = path.join(__dirname, '‘../..’', 'build');
 
-app.use(express.static(publicPath));
+app.use(express.static(buildPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 app.listen(port, () => {
