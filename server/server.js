0c43ef6f86111142450f7350fe0aa4306c717152
Shelby-Jean <shelby.gottschall@gmail.com>
2021-02-19 10:36:33 -0500

    updated server file

--- server/server.js
+++ server/server.js
@@ -4,15 +4,13 @@ const path = require('path');
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+const buildPath = path.join(__dirname, 'build');
 
-// const buildPath = path.join(__dirname, 'build');
+app.use(express.static(buildPath));
 
-// app.use(express.static(buildPath));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(buildPath, 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
