0e5e3b5dc14e0e09839e7b43ff1ffeeaea4b53a5
Shelby-Jean <shelby.gottschall@gmail.com>
2021-03-10 21:18:16 -0500

    commented out redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 import state from './state';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 
-const store = createStore(reducers, state, compose(applyMiddleware(thunk), composeWithDevTools()));
+// const store = createStore(reducers, state, compose(applyMiddleware(thunk), composeWithDevTools()));
+const store = createStore(reducers, state, compose(applyMiddleware(thunk)));
+
 
 export default store;
